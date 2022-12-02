e99fb6eef43e0f0be685c1647b788e41dbac55d9
Braxton Eddings <beddings81@gmail.com>
2022-12-01 23:25:38 -0600

    Add test for creating a new restaurant

--- spec/features/restaurants/new_spec.rb
+++ spec/features/restaurants/new_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'the Restaurant creation' do
-  it 'links to the new page from the restaurants index' do
-    
+  it 'links to the new page from the restaurants index' do   
     visit '/restaurants'
 
     click_link('New Restaurant')
 
     expect(current_path).to eq('/restaurants/new')
+  end
+
+  it 'can create a new restaurant' do
+    visit '/restaurants/new'
+
+    fill_in("name", with: "Sydney's Bakery")
+    click_button('Create Restaurant')
+
+    expect(current_path).to eq('/restaurants')
+    expect(page).to have_content("Sydney's Bakery")
   end
 end
