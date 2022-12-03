8f77ce9faac74bbe4d82b76f70fbc72300bbafb3
Braxton Eddings <beddings81@gmail.com>
2022-12-02 18:48:24 -0600

    Pass test for update restaurant link on restaurant show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/restaurants', to: 'restaurants#index'
   get '/restaurants/new', to: 'restaurants#new'
+  get '/restaurants/:id/edit', to: 'restaurants#update'
   get '/restaurants/:id', to: 'restaurants#show'
   get '/dishes', to: 'dishes#index'
   get '/dishes/:id', to: 'dishes#show'
