eb0a386ebb4940925c86f827bad9e8bf7fa53cfa
Braxton Eddings <beddings81@gmail.com>
2022-12-02 20:45:55 -0600

    Finish user story 12

--- spec/features/restaurants/update_spec.rb
+++ spec/features/restaurants/update_spec.rb
@@ -1,11 +1,3 @@
-
-# Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
-# When I fill out the form with updated information
-# And I click the button to submit the form
-# Then a `PATCH` request is sent to '/parents/:id',
-# the parent's info is updated,
-# and I am redirected to the Parent's Show page where I see the parent's updated info
-
 require 'rails_helper'
 
 RSpec.describe 'restaurants update page' do
