b7a33a460d09a4cc99a4d0e4532a4f0fc7dca248
Braxton Eddings <beddings81@gmail.com>
2022-12-04 15:30:26 -0600

    Add test for restaurant update link on index page

--- spec/features/restaurants/index_spec.rb
+++ spec/features/restaurants/index_spec.rb
@@ -1,29 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "restaurants index page", type: :feature do
-  it 'can see all restaurant names' do
-    restaurant = Restaurant.create(id: 1, created_at: DateTime.now,
+  before :each do
+    @restaurant = Restaurant.create(id: 1, created_at: DateTime.now,
      updated_at: DateTime.now, open: true, rating: 3, name: "Rocky Top Cafe")
 
-    restaurant1 = Restaurant.create(id: 2, created_at: DateTime.now,
+    @restaurant1 = Restaurant.create(id: 2, created_at: DateTime.now,
      updated_at: DateTime.now, open: true, rating: 2.5, name: "Eddings Cafe")
-
+  end
+  it 'can see all restaurant names' do
     visit "/restaurants"
 
-    expect(page).to have_content(restaurant.name)
-    expect(page).to have_content(restaurant1.name)
+    expect(page).to have_content(@restaurant.name)
+    expect(page).to have_content(@restaurant1.name)
   end
 
   it 'can see the restaurants created at' do
-    restaurant = Restaurant.create(id: 1, created_at: DateTime.now,
-     updated_at: DateTime.now, open: true, rating: 3, name: "Rocky Top Cafe")
-
-    restaurant1 = Restaurant.create(id: 2, created_at: DateTime.now,
-     updated_at: DateTime.now, open: true, rating: 2.5, name: "Eddings Cafe")
-
      visit "/restaurants"
 
-     expect(page).to have_content(restaurant.created_at)
-     expect(page).to have_content(restaurant1.created_at)
+     expect(page).to have_content(@restaurant.created_at)
+     expect(page).to have_content(@restaurant1.created_at)
+  end
+
+  it 'links to edit a restaurant' do
+    visit "/restaurants"
+
+    click_link("Update #{@restaurant.name}")
+    expect(current_path).to eq("/restaurants/#{@restaurant.id}/edit")
+    
+    visit "/restaurants"
+    click_link("Update #{@restaurant1.name}")
+    expect(current_path).to eq("/restaurants/#{@restaurant1.id}/edit")
   end
 end
