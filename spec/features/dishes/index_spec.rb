e7c77bf6ed4566a3df0f6bfa62ef5fd7215e8909
Braxton Eddings <beddings81@gmail.com>
2022-12-04 18:46:06 -0600

    Add test for link to a dishes edit page

--- spec/features/dishes/index_spec.rb
+++ spec/features/dishes/index_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "dishes index page", type: :feature do
+  before :each do
+    @restaurant = Restaurant.create!(open: true, rating: 3, name: "Rocky Top Cafe")
+    @dish = @restaurant.dishes.create!(hot: true, price: 15, name: "Seafood Pasta")
+    @dish2 = @restaurant.dishes.create!(hot: true, price: 25, name: "12oz Ribeye")
+    @dish3 = @restaurant.dishes.create!(hot: false, price: 25, name: "Im not true")
+  end
+
   it 'can see all the dishes and their attributes' do
-    restaurant = Restaurant.create!(id: 1, created_at: DateTime.now,
-     updated_at: DateTime.now, open: true, rating: 3, name: "Rocky Top Cafe")
-
-    dish = Dish.create!(id: 1, restaurant_id:1, created_at: DateTime.now,
-     updated_at: DateTime.now, hot: true, price: 15, name: "Seafood Pasta")
-
-    dish2 = Dish.create!(id: 2, restaurant_id: 1, created_at: DateTime.now,
-     updated_at: DateTime.now, hot: true, price: 25, name: "12oz Ribeye")
-
-    dish3 = Dish.create!(id: 3, restaurant_id: 1, created_at: DateTime.now,
-     updated_at: DateTime.now, hot: false, price: 25, name: "Im not true")
-
     visit "/dishes"
 
-    expect(page).to have_content(dish.name)
-    expect(page).to have_content(dish2.name)
-    expect(page).not_to have_content(dish3.name)
+    expect(page).to have_content(@dish.name)
+    expect(page).to have_content(@dish2.name)
+    expect(page).not_to have_content(@dish3.name)
   end
+
+  it 'links to a dishes edit page' do
+    visit "/dishes"
+
+    click_link("Update #{@dish.name}")
+
+    expect(current_path).to eq("/dishes/#{@dish.id}/edit")
+  end
+
 end
